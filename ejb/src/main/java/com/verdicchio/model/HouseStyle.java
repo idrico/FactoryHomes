9f68f38a0706319cdb69f59d651d7b48ee530ee3
Angelo Verdicchio <verdicchioangelo@gmail.com>
2015-11-07 13:31:34 +0100

    Commit to test the CI

--- ejb/src/main/java/com/verdicchio/model/HouseStyle.java
+++ ejb/src/main/java/com/verdicchio/model/HouseStyle.java
@@ -4,4 +4,13 @@ package com.verdicchio.model;
  * Created by anonymous on 07/11/15.
  */
 public class HouseStyle {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
